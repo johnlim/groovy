4c47ec69ef6e9d8548dc0e6cfb74eb68ac477d12
johnlim.dingwei@gmail.com <johnlim.dingwei@gmail.com>
2014-02-02 12:33:27 +0800

    Code cleanup

--- grails-app/controllers/gameoflife/WorldController.groovy
+++ grails-app/controllers/gameoflife/WorldController.groovy
@@ -23,29 +23,20 @@ class World{
 
   private int getNumberOfLiveNeighbours(Cell cell){
     int numberOfLiveNeighbours = 0;
-//    for(neighbours in cell.neighbours()){
-//      if(setOfLiveCells.contains(neighbours)){
-//        numberOfLiveNeighbours++;
-//      }   
-//    }   
     cell.neighbours().each{ if(isCellAlive(it)) numberOfLiveNeighbours++; }
     return numberOfLiveNeighbours; 
   }
 
   World tick(){
     World nextGeneration = new World();
-    //for (liveCell in setOfLiveCells){
+    def cellIsDead = {cell ->  setOfLiveCells.contains(cell) == false};
     setOfLiveCells.each{
         if((getNumberOfLiveNeighbours(it) == 2) || (getNumberOfLiveNeighbours(it) == 3)){
          nextGeneration.giveCellLife(it); 
         }
-    //}
-    //setOfLiveCells.each{
       it.neighbours().each{
-        if(setOfLiveCells.contains(it) != true){
-          if(getNumberOfLiveNeighbours(it) == 3){
+        if(cellIsDead(it) && getNumberOfLiveNeighbours(it) == 3){ 
             nextGeneration.giveCellLife(it);
-          }
         }
       }
     }
@@ -53,33 +44,17 @@ class World{
    }
 }
 
-//@groovy.transform.EqualsAndHashCode( includeFields = true)
+@TupleConstructor(includeFields = true)
 @Canonical
-//@EqualsAndHashCode( includeFields = true)
 class Cell implements Comparable<Cell>{
+  int x;
+  int y;
   
- /* private*/ int x;
- /* private*/ int y;
-  
-//  Cell(int _x, int _y){
-//    x = _x;
-//    y = _y;
-//  }
-
   int compareTo(Cell cell){
-//    if (x == cell.x && y == cell.y)
-//      return 0;
-//    if (x <= cell.x)
-//      return -1;
-//    else
-//      return 1;
-//   
     x <=> cell.x ?: y <=> cell.y ?: 0;
   }
 
   List neighbours(){
-    List index = [[-1,0], [1,0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]];
-    //List cellNeighbours = [[x-1, y],[x+1, y], [x, y-1], [x, y+1], [x-1, y-1], [x-1, y+1], [x+1, y-1], [x+1, y+1]];
     List cellNeighbours = [];
     cellNeighbours.add(new Cell(x-1, y));
     cellNeighbours.add(new Cell(x+1, y));
